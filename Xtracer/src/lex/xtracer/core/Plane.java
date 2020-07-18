da8400b1df3f097ff30fa0c9d33260fb9abc5b34
Lexinon <willi4wuff@gmail.com>
2020-07-18 14:43:08 +0200

    Moved Hittable methods from Plane to Plane.Hittable

--- Xtracer/src/lex/xtracer/core/Plane.java
+++ Xtracer/src/lex/xtracer/core/Plane.java
@@ -5,45 +5,6 @@ public class Plane implements Hittable {
 	public Vec3 point;
 	public Vec3 inwardNormalVector;
 	
-	@Override
-	public double intersectionTest(Ray ray) {
-		
-		try {
-			
-			double distance = (point.minus(ray.origin).dot(inwardNormalVector)) / ray.direction.dot(inwardNormalVector);
-			
-			if(distance <= 0) {
-				
-				return Double.MAX_VALUE;
-				
-			} else {
-				
-				return distance;
-				
-			}
-			
-		} catch(ArithmeticException e) {
-			
-			return Double.MAX_VALUE;
-			
-		}
-		
-	}
-	
-	@Override
-	public Vec3 getOutwardNormalVector(Vec3 point) {
-		
-		return new Vec3().minus(inwardNormalVector);
-		
-	}
-	
-	@Override
-	public Vec3 getInwardNormalVector(Vec3 point) {
-		
-		return inwardNormalVector;
-		
-	}
-	
 	public Plane(Vec3 point, Vec3 inwardNormalVector) {
 		this.point = point;
 		this.inwardNormalVector = inwardNormalVector;
@@ -51,7 +12,44 @@ public class Plane implements Hittable {
 	
 	public class Hittable implements Hittable {
 		
+		@Override
+		public double intersectionTest(Ray ray) {
+			
+			try {
+				
+				double distance = (point.minus(ray.origin).dot(inwardNormalVector)) / ray.direction.dot(inwardNormalVector);
+				
+				if(distance <= 0) {
+					
+					return Double.MAX_VALUE;
+					
+				} else {
+					
+					return distance;
+					
+				}
+				
+			} catch(ArithmeticException e) {
+				
+				return Double.MAX_VALUE;
+				
+			}
+			
+		}
 		
+		@Override
+		public Vec3 getOutwardNormalVector(Vec3 point) {
+			
+			return new Vec3().minus(inwardNormalVector);
+			
+		}
+		
+		@Override
+		public Vec3 getInwardNormalVector(Vec3 point) {
+			
+			return inwardNormalVector;
+			
+		}
 		
 	}
 	
